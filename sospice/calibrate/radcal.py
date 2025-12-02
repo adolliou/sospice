ebffb3998a976e43f936dceed48bc9c00d6a98d1
adolliou <antoine.dolliou@universite-paris-saclay.fr>
2025-12-02 19:17:31 +0100

    Added the get_radcal wrapper function, and added tests

--- sospice/calibrate/radcal.py
+++ sospice/calibrate/radcal.py
@@ -5,6 +5,93 @@ from astropy.wcs import WCS
 import numpy as np
 
 
+def get_radcal(
+    window: int | str,     
+    hdul: fits.HDUList | None       = None,
+    path_to_l2: str    | None       = None,
+    return_base_wavelength: bool    = False,):
+    """
+    Return the radiometric calibration of a specific spectral window for an SPICE fits file.
+    Accepts either an HDUList or the path to a L2 FITS file.
+
+    Parameters
+    ----------
+    window: int or str
+        index or extname of the spectral window in the HDUList 
+    hdul: fits.HDUList (optional)
+        HDUList of a SPICE FITS file
+    path_to_l2: str
+        path to a L2 SPICE fits file
+    return_base_wavelength: bool
+        set to True to return the wavelength array corresponding to the radiometric calibration
+    Return
+    ------
+    ND array radiometric calibration array for the given spectral window, 
+    (optional) ND array associated wavelength array
+
+    """
+    if ((hdul is None) & (path_to_l2 is None)) or ((hdul is not None) & (path_to_l2 is not None)):
+        raise ValueError("Only one parameter (hdul or path_to_l2) must be provided")
+    
+    if path_to_l2 is not None:
+        hdul = fits.open(path_to_l2)
+    hdu = hdul[window]
+    hdu_binary = hdul["VARIABLE_KEYWORDS"]
+    if return_base_wavelength:
+        radcal_array, wavelength_array = get_radcal_from_hdus(hdu=hdu,
+                                                              hdu_binary=hdu_binary,
+                                                              return_base_wavelength=return_base_wavelength)
+        if path_to_l2 is not None:
+            hdul.close()
+        return radcal_array, wavelength_array
+    else:
+        radcal_array = get_radcal_from_hdus(hdu=hdu,
+                                            hdu_binary=hdu_binary,
+                                            return_base_wavelength=return_base_wavelength)
+        if path_to_l2 is not None:
+            hdul.close()        
+        return radcal_array 
+    
+    
+
+
+
+def get_radcal_from_hdus(hdu: fits.ImageHDU, hdu_binary: fits.BinTableHDU, return_base_wavelength: bool = False):
+
+    """
+    Return the radiometric calibration array for the corresponding spectral window HDU, and binary table. 
+
+    Parameters
+    ----------
+    hdu: fits.ImageHDU
+        HDU of the given spectral window
+    binary_table: tuple
+        VARIABLE_KEYWORDS of the corresponding HDUList
+    return_base_wavelength: bool
+        set to True to return the wavelength array corresponding to the radiometric calibration
+    Return
+    ------
+    ND array radiometric calibration array for the given spectral window
+
+    """    
+
+    if (hdu.header["EXTNAME"] == "VARIABLE_KEYWORDS"):
+        raise ValueError("The input hdu must correspond to a spectral window")
+
+    radcal_ttype    = _get_radcal_ttype(hdu.header)
+    radcal_array    = np.squeeze(hdu_binary.data[radcal_ttype])
+    if return_base_wavelength:
+        w = WCS(hdu.header)
+        w_spec = w.spectral
+        x = np.arange(hdu.data.shape[1])
+        wavelength_array = w_spec.pixel_to_world(x)
+        return radcal_array, wavelength_array
+    else:
+        return radcal_array
+
+
+
+
 
 def _get_radcal_ttype(header_l2: fits.header):
     """
@@ -39,36 +126,4 @@ def _get_radcal_ttype(header_l2: fits.header):
         raise KeyError("radcal ttype could not be found in the VAR_KEYS keyword value.")
     radcal_ttype = radcal_ttype_list[0]
     return radcal_ttype
-
-
-
-
-
-def get_radcal_from_hdus(hdu: fits.ImageHDU, hdu_binary: fits.BinTableHDU, return_base_wavelength: bool=False):
-
-    #TODO assert hdu to be for a spectral window.
-
-    radcal_ttype    = _get_radcal_ttype(hdu.header)
-    radcal_array    = np.squeeze(hdu_binary.data[radcal_ttype])
-    if return_base_wavelength:
-        w = WCS(hdu.header)
-        w_spec = w.spectral
-        x = np.arange(hdu.data.shape[1])
-        wavelength_array = w_spec.pixel_to_world(x)
-        return radcal_array, wavelength_array
-    else:
-        return radcal_array
-
-
-
-
-
-# if __name__ == "__main__":
-#     url = ("https://spice.osups.universite-paris-saclay.fr/spice-data/"
-#     "release-5.0/level2/2022/04/02/solo_L2_spice-n-ras_20220402T111537_V22_100664002-000.fits")
-#     with fits.open(url) as hdul:
-#         hdu = hdul[0]
-#         hdu_bin = hdul["VARIABLE_KEYWORDS"]
-
-#         get_radcal_from_hdus(hdu, hdu_bin)
 
